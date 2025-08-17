56cd3399d3b581949a7c3df0b75b24a22abceff5
Huy Nguyen <kten90672@gmail.com>
2025-08-18 00:23:48 +0700

    Update get_recommended_filters.sh

--- get_recommended_filters.sh
+++ get_recommended_filters.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-node download_lists.js blocklist
+# declare an array of urls
+urls=(
+  https://raw.githubusercontent.com/mullvad/dns-blocklists/main/output/doh/doh_adblock.txt
+  https://raw.githubusercontent.com/mullvad/dns-blocklists/main/output/doh/doh_gambling.txt
+  https://raw.githubusercontent.com/mullvad/dns-blocklists/main/output/doh/doh_privacy.txt
+  https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.Risk/hosts
+  https://adaway.org/hosts.txt
+  https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
+  https://raw.githubusercontent.com/bigdargon/hostsVN/master/option/domain.txt
+  https://raw.githubusercontent.com/hagezi/dns-blocklists/main/wildcard/multi.txt
+)
+
+# download all files in parallel and append them to blocklist.txt
+node download_lists.js blocklist.txt ${urls[@]}
+
+# print a message when done
+echo "Done. The blocklist.txt file contains merged data from recommended filter lists."
